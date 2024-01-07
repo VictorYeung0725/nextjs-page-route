f8335334a0321b9c8adf6b3a25c222fe5cf32b7a
VictorYeung0725 <96713542+VictorYeung0725@users.noreply.github.com>
2024-01-07 14:12:28 +0800

    completed the learning on page router in next.js

--- pages/client/[id]/index.js
+++ pages/client/[id]/index.js
@@ -3,12 +3,22 @@ function ClientProjectPage() {
   const router = useRouter();
   console.log(router.query);
 
-  function 
+  function loadProjectHandler() {
+    //all sort of action load data ...
+    // router.push('/client/max/projecta');
+    // router.push({
+    //   pathname: '/client/[id]/[clientProjectId]',
+    //   query: { id: 'max', clientProjectId: 'projecta' },
+    // });
+
+    //replace the current Url, we cant go back after the navigation
+    router.replace('/client/victor/projecta');
+  }
   return (
     <div>
       <h1>The Project of a given Client</h1>
 
-      <button>Load Project A</button>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
